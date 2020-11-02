02d9b8701d9de5012948fedafbfe65a3c26446da
jetsonhacks <jim@jetsonhacks.com>
2020-11-01 21:04:45 -0800

    Initial Commit

---
+++ installLXDE.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install LXDE (Lightweight Desktop Environment) on a NVIDIA Jetson Developer Kit
+# Install the LightDM desktop manager
+# Use Compton desktop compositor 
+sudo apt-get update
+sudo apt-get install lightdm lxde compton -y
+
+# Write the following text to the file /etc/xdg/autostart/lxde-compton.desktop
+# This tells LXDE to use the Compton compositor
+sudo dd of=/etc/xdg/autostart/lxde-compton.desktop << EOF
+[Desktop Entry]
+Type=Application
+Name=Compton (X Compositor)
+GenericName=X compositor
+Comment=A X compositor
+TryExec=compton
+Exec=compton --backend glx -b
+OnlyShowIn=LXDE
+EOF
+
+# Use the lightdm desktop manager instead of gdm3
+# lxde works with either lightdm or gdm3; If you want to use gdm3, comment this line out
+sudo debconf-set-selections <<< "lightdm shared/default-x-display-manager select lightdm"
+
+# You can manually change desktop managers using:
+# sudo dpkg-reconfigure lightdm
+#
+# or the equivalent:
+# 
+# sudo dkpg-reconfigure gdm3
+
+echo 'Please reboot for changes to take effect'
